1bd346af96a1d62f8165adf831b44d64b43d58b4
hasan814 <h.mousavi910@gmail.com>
2024-10-04 13:18:06 +0330

    Added Header Page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,7 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
+import Provider from "@/providers/Provider";
+
 import "./globals.css";
 
 const geistSans = localFont({
@@ -28,7 +30,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <Provider>{children}</Provider>
       </body>
     </html>
   );
---
+++ src/components/layouts/Header.tsx
@@ -0,0 +1,64 @@
+"use client";
+
+import { usePathname } from "next/navigation";
+import { Button } from "@/components/ui/button";
+import { FaPlus } from "react-icons/fa6";
+
+import Image from "next/image";
+import Link from "next/link";
+
+const Header = () => {
+  // ============ PathName =============
+  const path = usePathname();
+
+  // ============ Rendering =============
+  return (
+    <div className="p-3 px-10 flex justify-between items-center borde shadow-lg fixed top-0 w-full z-10 bg-white">
+      <div className="flex items-center gap-12">
+        <Image
+          src={"https://img.icons8.com/color/96/city-block.png"}
+          alt="logo"
+          width={60}
+          height={60}
+        />
+        <ul className="hidden md:flex gap-10">
+          <Link href="/">
+            <li
+              className={`${
+                path === "/" ? "text-primary" : ""
+              } hover:text-primary font-medium text-sm cursor-pointer`}
+            >
+              For Sale
+            </li>
+          </Link>
+          <Link href="/for-rent">
+            <li
+              className={`${
+                path === "/for-rent" ? "text-primary" : ""
+              } hover:text-primary font-medium text-sm cursor-pointer`}
+            >
+              For Rent
+            </li>
+          </Link>
+          <Link href="/agent-finder">
+            <li
+              className={`${
+                path === "/agent-finder" ? "text-primary" : ""
+              } hover:text-primary font-medium text-sm cursor-pointer`}
+            >
+              Agent Finder
+            </li>
+          </Link>
+        </ul>
+      </div>
+      <div className="flex gap-3">
+        <Button className="flex gap-2">
+          <FaPlus className="h-5 w-5" /> Post Your AD
+        </Button>
+        <Button variant={"outline"}>Login</Button>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
